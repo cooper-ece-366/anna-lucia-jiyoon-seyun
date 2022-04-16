500329ba4116dde39bf98669027243ad3bbfa238
Lucia Rhode <l.h.rhode@gmail.com>
2022-04-16 17:38:23 -0400

    commit to clean out server

--- TonePoemService/src/main/java/edu/cooper/ece366/Main.java
+++ TonePoemService/src/main/java/edu/cooper/ece366/Main.java
@@ -1,2 +1,13 @@
-package edu.cooper.ece366;public class Main {
+package edu.cooper.ece366;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        get("/test", (req,res)->{
+            return "Woo, we love TonePoem";
+        });
+
+    }
 }
