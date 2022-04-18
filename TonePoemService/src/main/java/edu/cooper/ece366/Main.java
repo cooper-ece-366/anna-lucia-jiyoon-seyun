4b7fc915d349eb93a87144200963b5df43b2eb20
Lucia Rhode <l.h.rhode@gmail.com>
2022-04-18 18:51:37 -0400

    swap from legacy mongo driver to modern mongo driver create mongo handler call mongo handler start a mongo server make an image of the mongo server with the mongodb port docker-compose up start backend server can connect to mango database!
    try storing/inserting in mango database
    database testing works!

--- TonePoemService/src/main/java/edu/cooper/ece366/Main.java
+++ TonePoemService/src/main/java/edu/cooper/ece366/Main.java
@@ -1,6 +1,20 @@
 package edu.cooper.ece366;
 
 import static spark.Spark.*;
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.MongoException;
+import edu.cooper.ece366.Mongo.MongoHandler;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.types.ObjectId;
+
+import java.util.Arrays;
 
 public class Main {
 
@@ -8,6 +22,23 @@ public class Main {
         get("/test", (req,res)->{
             return "Woo, we love TonePoem";
         });
+        MongoHandler handler = new MongoHandler("HelloWorld");
+        MongoCollection<Document> collection = handler.getCollection("Earth");
 
+        try{
+            Bson command = new BsonDocument("ping", new BsonInt64(1));
+            Document commandResult = handler.db.runCommand(command);
+            System.out.println("Connected successfully to server.");
+            InsertOneResult result = collection.insertOne(new Document()
+                    .append("_id", new ObjectId())
+                    .append("Country", "Taiwan")
+                    .append("Foods", Arrays.asList("Bubble Tea", "Pineapple Cake"))
+            );
+            System.out.println("Got id: " + result.getInsertedId());
+            Document doc = collection.find(eq("Country", "Taiwan")).first();
+            System.out.println("Got: " + doc.toJson());
+        } catch (Exception e){
+            System.err.println("Uh oh, error");
+        }
     }
 }
