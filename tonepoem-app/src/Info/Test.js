188eb9f6aeb02dbc6961f61a01ca51b29592c613
Rob Marano <robmarano@gmail.com>
2022-04-18 20:20:06 -0400

    Added useful but not working code on src/Info/Test.js

--- tonepoem-app/src/Info/Test.js
+++ tonepoem-app/src/Info/Test.js
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/67254778/how-to-get-onclick-event-for-a-label-in-chartjs-and-react
 import React, { Component, MouseEvent, useRef } from 'react'
 //import type { InteractionItem } from 'chart.js';
 import {
@@ -20,39 +21,52 @@ ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
 //const chartRef = useRef();
 
+var chartData = {
+  labels: ['Adjective 1', 'Adjective 2', 'Adjective 3', 'Adjective 4', 'Adjective 5', 'Adjective 6'],
+  datasets: [
+      {
+        label: 'Number of Selection',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132)',
+          'rgba(54, 162, 235)',
+          'rgba(255, 206, 86)',
+          'rgba(75, 192, 192)',
+          'rgba(153, 102, 255)',
+          'rgba(255, 159, 64)',
+        ],
+        borderWidth: 1,
+      },
+    ]
+};
+
+
 export default class Test extends Component {
     constructor(props) {
         super(props);
         this.state = {
             active: true,
-            new: false
+            new: false,
+            data: props.chartData
         }
-//        this.onClicking = this.onClicking.bind(this)
+        this.onClicking = this.onClicking.bind(this)
     }
-
-    data = {
-        labels: ['Adjective 1', 'Adjective 2', 'Adjective 3', 'Adjective 4', 'Adjective 5', 'Adjective 6'],
-        datasets: [
-            {
-              label: 'Number of Selection',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)',
-                'rgba(75, 192, 192)',
-                'rgba(153, 102, 255)',
-                'rgba(255, 159, 64)',
-              ],
-              borderWidth: 1,
-            },
-          ]
-    };
 
 //    chartRef = useRef();
 //    onClicking = (event) => {
 //       console.log('hello');
 //    };
+
+    onClicking() {
+      console.log("onClicking() ***");
+      console.log(this.data);
+    }
+
+
+componentDidMount() {
+  console.log("componentDidMount(): ----");
+}
+
 
     // REFER TO: https://react-chartjs-2.js.org/docs/working-with-events
     // TODO: React Chartjs with class version
@@ -60,12 +74,19 @@ export default class Test extends Component {
     render(){
         return (
             <PolarArea
-                data={this.data} 
-//                options = {{
-//                  onClick: () => {alert("test")}}
-//                }
-                // ref={this.chartRef}
-                
+              data={this.state.data}
+               options = {{
+                onClick: function(evt, element) {
+                  // onClickNot working element null
+                  console.log(evt, element);
+                  if (element.length > 0) {
+                    console.log(element, element[0]._datasetInde);
+                    // you can also get dataset of your selected element
+                    console.log(data.datasets[element[0]._datasetIndex]);
+               }}
+              }
+            }
+                // ref={this.chartRef}  
             />)
     }
 }
