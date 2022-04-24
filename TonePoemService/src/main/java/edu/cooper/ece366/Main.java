3d4d3e093a5c77705734b576a137003e2941e9e9
Lucia Rhode <l.h.rhode@gmail.com>
2022-04-24 19:22:08 -0400

    set up user hander and middleware to authenticate

--- TonePoemService/src/main/java/edu/cooper/ece366/Main.java
+++ TonePoemService/src/main/java/edu/cooper/ece366/Main.java
@@ -5,6 +5,8 @@ import static com.mongodb.client.model.Filters.eq;
 
 import com.mongodb.MongoException;
 import edu.cooper.ece366.Mongo.MongoHandler;
+import edu.cooper.ece366.Mongo.User.User;
+import edu.cooper.ece366.Mongo.User.UserHandler;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
@@ -40,5 +42,24 @@ public class Main {
         } catch (Exception e){
             System.err.println("Uh oh, error");
         }
+        try{
+            UserHandler handled = new UserHandler(handler);
+            User user = handled.verifyUser("eyJhbGciOiJSUzI1NiIsImtpZCI6ImQzMzJhYjU0NWNjMTg5ZGYxMzNlZmRkYjNhNmM0MDJlYmY0ODlhYzIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiOTUyOTcyNTA1ODUzLWQwY3Q0am5hNGtwNXRvbHNtODh2aDU1dW82NHA3NnViLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiOTUyOTcyNTA1ODUzLWQwY3Q0am5hNGtwNXRvbHNtODh2aDU1dW82NHA3NnViLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTAwOTU5MzMzNzcxNzk4OTgzMTg0IiwiZW1haWwiOiJsLmgucmhvZGVAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJOcnVSSFZyRWQxOFJlOVpXQi1ZUFd3IiwibmFtZSI6Ikx1Y2lhIFJob2RlIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FBVFhBSndWLVdVRVZDU2VYZ2VHWmhLM2tjQ0NkbjBYWHdmTGlhSms3UlJoPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6Ikx1Y2lhIiwiZmFtaWx5X25hbWUiOiJSaG9kZSIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNjUwODMyODQxLCJleHAiOjE2NTA4MzY0NDEsImp0aSI6IjYyZmZmYTMxOTM5NzhiMDgyNjA4MGFjNzRkMTE1NTI0NTU5Nzk3ZWEifQ.D7MKU-OQT4rtSCmiLWZEE4rBpaAeKl2kjeELs6_zwHXD5hnRAtPWvpcbD7STHXLuMenWi9Ca9VrCdMVzXfElCznHNCK2zZ-rGnObLRWQYkQkKCAi8zRSdPbdMtmOMETvW7ClVBPxx0IzEQ15uAj-j82UIJ67CPKtCdXPA6v5OqeiGVgnpV3vXHSzwKNDJ6ccaNQBBIXt7u8g4w7X_32OXrtRwMS0qn_sXZmtq6IKXGrfsi_qzLXlZ3P_djzcRtvvlnIjCDOY1WW5oPURPFcC4yaZ-xYSkaz0maeUqNJUatSOKtufVIl8aeLBQ6zfFU_2Os5F2Dag4kjeylX00hatWA");
+            System.out.println(user);
+
+        } catch (Exception e){
+            System.err.println("Uh oh, no login");
+        }
+
+        get("/hello", (req,res) -> {
+            try{
+                UserHandler handled = new UserHandler(handler);
+                User user = handled.verifyUser(req.headers("Authorization"));
+                return user;
+            } catch (Exception e){
+                System.err.println("Uh oh, no login");
+            }
+            return "";
+        });
     }
 }
--- TonePoemService/src/main/java/edu/cooper/ece366/Mongo/User/User.java
+++ TonePoemService/src/main/java/edu/cooper/ece366/Mongo/User/User.java
@@ -1,2 +1,34 @@
-package edu.cooper.ece366.Mongo.User;public class User {
+package edu.cooper.ece366.Mongo.User;
+// from Greg :)
+import com.google.gson.Gson;
+import org.bson.codecs.pojo.annotations.BsonCreator;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
+// user POJO that is seralizedable to JSON
+public class User {
+    @BsonProperty("_id") public final String userId;
+    @BsonProperty("firstName") public final String firstName;
+    @BsonProperty("lastName") public final String lastName;
+    @BsonProperty("email") public final String email;
+
+    @BsonCreator
+    public User(
+            @BsonProperty("_id") String userId,
+            @BsonProperty("firstName") String firstName,
+            @BsonProperty("lastName") String lastName,
+            @BsonProperty("email") String email
+    ) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String toJSONString() {
+        return new Gson().toJson(this);
+    }
+
+    public String toString() {
+        return toJSONString();
+    }
 }
