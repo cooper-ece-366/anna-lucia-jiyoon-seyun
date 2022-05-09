80da30d12f14a6452f6b689e52f970f68b47bf0e
Lucia Rhode <l.h.rhode@gmail.com>
2022-05-09 17:41:38 -0400

    frontend login work!

--- tonepoem-app/src/navbar/login/Login.js
+++ tonepoem-app/src/navbar/login/Login.js
@@ -7,18 +7,30 @@ const Login = () => {
     const responseGoogle = (response) => {
       console.log(response);
       localStorage.setItem('login_token', response.tokenId);
+      window.location.reload();
+    }
+
+    const Logout = () => {
+        localStorage.removeItem('login_token');
+        window.location.reload();
+    }
+
+    if (window.localStorage.getItem("login_token") === null) {
+        return (
+            <GoogleLogin
+                clientId="952972505853-d0ct4jna4kp5tolsm88vh55uo64p76ub.apps.googleusercontent.com"
+                buttonText="Login"
+                render={renderProps => (
+                    <button className="mr-[35px] hover:bg-[#232A4E] border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>)}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+        )
     }
 
     return(
-        <GoogleLogin
-            clientId="952972505853-d0ct4jna4kp5tolsm88vh55uo64p76ub.apps.googleusercontent.com"
-            buttonText="Login"
-            render={renderProps => (
-                <button className="mr-[35px] hover:bg-[#232A4E] border-[2px] rounded-[10px] border-[#232A4E] px-[25px] py-[7px]" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>)}
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-          />
+        <button onClick={Logout}>Logout</button>
     )
 }
 export default Login;
