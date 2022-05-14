90cdb2cd974d514d16d292bd312abdf0eb6186ea
MELLI <99melliuni@gmail.com>
2022-05-14 18:04:25 -0400

    changed explore page button, modifying UI

--- tonepoem-app/src/Components/exploreUtils/AudioWordClouds.js
+++ tonepoem-app/src/Components/exploreUtils/AudioWordClouds.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
-import MiniPlayer from '../mini_audio_player/MiniPlayer'
+import React, {useState} from 'react';
+import MiniPlayer from '../mini_audio_player/MiniPlayer';
+import HorizPlayer from '../HorizPlayer/HorizPlayer';
 import styled from 'styled-components';
 
 import {
@@ -16,52 +17,24 @@ const Button = styled.button`
     outline: 0;
     box-shadow: 0px 1.2px 1.2px black;
 `
-  
-
-const AudioWordClouds = ({ sounds, loading}) => {
-
-    const [id, setId] = useState();
-    const history = useHistory();
-
-    const handleProceed = (event) => {
-        console.log(id);
-        id && history.push(generatePath("/tag/:id", { id }));
-    }
-    console.log(id);
+const AudioWordClouds = ({ sounds, loading }) => {
     return (
-        
-
         <div>
             <div className="list-group flex flex-col justify-center items-center" >
                     {sounds.map(sound => {
-                        
                         return(
-                            
                             <div key={sound._id} className="nothing flex flex-row justify-center items-center text-sm">
-                            <MiniPlayer
+                            <HorizPlayer
                                 song={sound}
                             />                    
-
-                            <Button className="button w-[100px]" 
-                            onClick={() => 
-                                {setId(sound._id);
-                                handleProceed();
-                                }
-                            }>
+                            <Button className="button w-[100px] flex justify-center items-center ml-[10rem]" 
+                                onClick={(e) => 
+                                    {e.preventDefault();
+                                        window.location.href=`/explore/${sound._id}`;
+                                    }
+                                }>
                                 <strong>More</strong>
-                            </Button> 
-
-                            {/* <button onClick={handleProceed}>
-                                More
-                            </button> */}
-
-                            {/* <a href="http://localhost:3000/tag/${}">
-                                <button onClick={handleProceed}>
-                                    More
-                                </button>
-                            </a> */}
-
-                            
+                            </Button>                             
                         </div>
                     )
                        
@@ -73,22 +46,3 @@ const AudioWordClouds = ({ sounds, loading}) => {
 }
 
 export default AudioWordClouds
-
-
-// const Navigation = () => {
-//     return (
-//         <div>
-//             <Router>
-//                 <Switch>
-//                     <Route path="/tag/:id">
-//                         <AudioWordClouds />
-//                     </Route>
-//                     {/* <Route path="/">
-//                         <
-//                     </Route> */}
-//                 </Switch>
-//             </Router>
-//         </div>
-
-//     )
-// }
