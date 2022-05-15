4754157087a753c272a1b53894262783eae30e40
Anna <a.m.konvicka@gmail.com>
2022-05-15 17:20:11 -0500

    Finalized "Learn More" page

--- tonepoem-app/src/learn_more/Learn_more.js
+++ tonepoem-app/src/learn_more/Learn_more.js
@@ -3,59 +3,45 @@ import React from 'react' // THIS LINE IS REQUIRED
 //Function coded by: Anna Konvicka
 const Learn_more = () => {
     return (
-        //<div className="experience flex flex-col items-center justify-start px-[5rem] bg-[#020917] h-[60rem] pt-[18rem] mt-[-10rem] relative z-[2] rounded-b-[5rem]">
-        //    <div className="headline mt-7 flex flex-col items-center text-[2rem]">
-        <div className="learn_more flex flex-col items-center justify-start px-[5rem] bg-[#020917] h-[60rem] pt-[18rem] mt-[-10rem] relative z-[2] rounded-b-[5rem]">
-            <div className="headline mt-7 flex flex-col items-center text-[3rem]">
-                <span> <b>Learn More</b> </span>
+        <div className="Contact px-[5rem] py-[2rem]">
+            <div className="text-2xl">
+                Learn More
             </div>
 
-            <div>
+            <div className="items-center text-[1rem]">
             <span>
-                Below are some resources we referenced and used conceptually in developing this project
+                <em>Below are some resources we referenced conceptually in developing this project:</em>
             </span>
             </div>
 
-            <div className="headline mt-5 flex flex-col items-center text-[2rem]">
-                <span> Ongoing Work </span>
+            <div className="headline mt-5 flex flex-col items-center text-[1.25rem]">
+                <b><span> Ongoing Work  <a className="text-muted text-indent 40px" href="https://steinhardt.nyu.edu/marl/research/projects/sounds-new-york-city-sonyc">  - SONYC </a> </span></b>
             </div>
             <div>
                 <span>
-                    The Steinardt School at NYU is working on SONYC, an ongoing project to monitor and classify features of the acoustic environment in NYC, with a focus on mitigation.
+                    The Steinardt School at NYU is working on SONYC, an ongoing project to monitor and classify features of the acoustic environment in NYC, with a focus on mitigation. As part of this project, they have produced a tagged, open-source sound library of the NYC cityscape.
                 </span>
             </div>
-            <div>
-                <a href="https://steinhardt.nyu.edu/marl/research/projects/sounds-new-york-city-sonyc">SONYC website</a>
-            </div>
 
-
-            <div className="headline mt-5 flex flex-col items-center text-[2rem]">
-                <span> Exploring Audiospace </span>
+            <div className="headline mt-5 flex flex-col items-center text-[1.25rem]">
+                <b><span> Exploring Audiospace <a className="text-muted text-indent 40px" href="https://everynoise.com/">  -   Every Noise at Once</a> </span></b>
             </div>
             <div>
                 <span>
                     We were inspired by exploratory methods of classification similar to those of the Spotify algorithm based on using labels to create categories of similar sounds.
                 </span>
             </div>
+
+            <div className="headline mt-5 flex flex-col items-center text-[1.25rem]">
+                <b><span> Tagging a Large Statespace <a className="text-muted" href="https://colornames.org/">  - Colornames</a> </span></b>
+            </div>
             <div>
-                <a href="https://everynoise.com/">Every Noise at Once</a>
+                <span>
+                    This open-source playful app encourages users to create inventive descriptions of the entire RGB colorspace with a tool that allows exploration of recent tags.
+                </span>
             </div>
-
-
-            <div className="audiospace py-[0.7rem]">
-                <div className="audiospace_title text-[0.9rem] py-[0.5rem]">
-                    Exploring Audiospace
-                </div>
-
-            </div>
-            <div className="topic_work py-[0.7rem]">
-                <div className="work_title text-[0.9rem] py-[0.5rem]">
-                    Other Work
-                </div>
-
-            </div>
-
         </div>
+
     );
 }
 
