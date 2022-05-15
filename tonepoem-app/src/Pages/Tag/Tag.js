d87c64ee61ab9f3f63bef920625f0516d776de59
MELLI <99melliuni@gmail.com>
2022-05-15 17:49:36 -0400

    word-cloud modified, picture size modified to get the login button pressing (in terms of UI)

--- tonepoem-app/src/Pages/Tag/Tag.js
+++ tonepoem-app/src/Pages/Tag/Tag.js
@@ -39,15 +39,6 @@ export const data = {
         "rgba(255, 159, 64, 1)",
         "rgba(54, 162, 235, 1)",
       ],
-      // borderColor: [
-      //   "rgba(255, 99, 132, 1)",
-      //   "rgba(54, 162, 235, 1)",
-      //   "rgba(255, 206, 86, 1)",
-      //   "rgba(75, 192, 192, 1)",
-      //   "rgba(153, 102, 255, 1)",
-      //   "rgba(255, 159, 64, 1)",
-      //   "rgba(54, 162, 235, 1)",
-      // ],
       borderWidth: 1,
     },
   ],
@@ -136,20 +127,13 @@ const Tag = () => {
             onClick={(e) => {
               e.preventDefault();
               axios.put(`http://localhost:8080/api/sounds/${_id}/${selection}`);
+              window.location.href=`/explore/${_id}`;
             }}
           >
             <strong>Submit</strong>
           </Button>
         </div>
       </div>
-      {/* <div className="flex items-right justify-right">
-          <div className="top">
-          
-          </div>
-          <div className="bottom">
-
-          </div>
-        </div> */}
     </div>
   );
 };
